267af94dd91be72799a7e7ad6d3c5fa89391d1ed
jonasgaia <jonasgaia.usd@gmail.com>
2025-05-20 21:11:30 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main (String[]args) {
+        Carro meucarro = new
+        Carro ("mustang v8 turbo   ");
+        meucarro.acelerar();
+        meucarro.acelerar();
+        meucarro.frear();
+    }
+}
